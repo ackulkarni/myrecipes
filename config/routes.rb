3da59f3d50cd4d88154e3961e858971bbe0f3216
Ack <arundhati.c.kulkarni@gmail.com>
2018-05-31 16:18:52 +0000

    complete comment feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,9 @@ Rails.application.routes.draw do
   
   get 'pages/home', to: 'pages#home'
   
-  resources :recipes
-  # get '/recipes', to: 'recipes#index'
-  # get '/recipes/new' , to: 'recipes#new' , as: 'new_recipe'
-  # get '/recipes/:id' , to: 'recipes#show' , as: 'recipe'
-  # post 'recipes', to: 'recipes#create'
+  resources :recipes do 
+    resources :comments, only: [:create]
+  end
   
   get '/signup' , to: 'chefs#new'
   resources :chefs, except: [:new]
